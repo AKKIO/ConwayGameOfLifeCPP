0fdefdd270cb3b61ebb61e71adb9dde8e394009a
ynwrdcreso <jaimeazael@live.com.mx>
2021-09-09 19:41:17 -0700

    final
    version 1 finalizada
    //
    version 1 done

--- GameOfLifeConway.cpp
+++ GameOfLifeConway.cpp
@@ -2,13 +2,17 @@
 #include <windows.h>
 #include <unistd.h>
 
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 
-const int width = 11;
-const int height = 11;
+const int width = 29;
+const int height = 29;
 
 int calGrid[width][height];
 int disGrid[width][height];
+
 
 int endSim = 0;
 
@@ -43,24 +47,82 @@ void drawGrid(){
 	}
 }
 
+int sumCells(int x, int y){
+	int cellNumber = 0;
+	cellNumber =   (disGrid[x+1][y] + disGrid[x][y+1] +
+					disGrid[x-1][y] + disGrid[x][y-1] +
+					disGrid[x+1][y+1] + disGrid[x+1][y-1] +
+					disGrid[x-1][y-1] + disGrid[x-1][y+1]);
+	//cout << endl << cellNumber;
+	return(cellNumber);
+}
+
+void checkCells(){
+	for (int y = 1; y < height-1; y++){
+	 	for (int x = 1; x < width-1; x++){
+	 		if (disGrid[x][y] == 1){
+	 			if (sumCells(x, y) == 3 || sumCells(x, y) == 2){
+	 				calGrid[x][y] = 1;
+	 			}else{
+	 				calGrid[x][y] = 0;
+	 			}
+	 		}else{
+	 			if (sumCells(x, y) == 3){
+	 				calGrid[x][y] = 1;
+	 			}
+	 		}
+	 	}
+	 }
+
+	for (int y = 1; y < height-1; y++){
+		for (int x = 1; x < width-1; x++){
+			disGrid[x][y] = calGrid[x][y];
+		}
+	}
+
+	//if (sumCells(5, 5))
+	//cout << sumCells(5, 5) << endl;
+}
 
 int main () {
 	ShowConsoleCursor(false);
 	SetConsoleOutputCP(CP_UTF8);
+
+	srand(time(0));
 	
-	fill(disGrid[0] + 0, disGrid[0]+ (width*height), 0);
+	fill(calGrid[0] + 0, calGrid[0]+ (width*height), 0);
+
+	// calGrid[4][5] = 1;
+	// calGrid[5][5] = 1;
+	// calGrid[6][5] = 1;
+	// calGrid[5][4] = 1;
+
+	for (int y = 1; y < height-1; y++){
+		for(int x = 1; x < width-1; x++){
+			int r;
+			r = rand() % 11;
+
+			if (r > 5){
+				calGrid[x][y] = 1;
+			}
+		}
+	}
+
+	for (int y = 0; y < height; y++){
+		for(int x = 0; x < width; x++){
+			disGrid[x][y] = calGrid[x][y];
+		}
+	}
 
 	drawGrid();
-	ClearScreen();
 
 	while (endSim != 1){
-		disGrid[width/2][width/2] = !disGrid[width/2][width/2];
-		drawGrid();
-
-
+		usleep(5000);
 		ClearScreen();
-		usleep(200000);
 
+		checkCells();
+
+		drawGrid();
 	}
+	return 0;
 }
-//save
